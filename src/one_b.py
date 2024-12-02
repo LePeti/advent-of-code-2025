adec00b1aee84be3b930e9e1a21db92c3ca4abc6
Peter Lukacs <lukacs.peter.andras@gmail.com>
2024-12-02 20:31:12 +0100

    day 1 / part 2

---
+++ src/one_b.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+from input.input_one_a import left_list, right_list
+
+if __name__ == "__main__":
+    right_counter = Counter(right_list)
+
+    solution = sum([left_number * right_counter.get(left_number, 0) for left_number in left_list])
+
+    print(solution)
